cdf67c8928d57621830c3b1d0f3245d477006701
VivoBook S15 <cchsieh19@gmail.com>
2026-03-19 21:58:49 +0800

    add auto move in / out function

---
+++ ProGitForProgrammers/ProGitForProgrammers/ASEFunc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+	internal class ASEFunc
+	{
+		public void AutoMvoeIn()
+		{
+
+		}
+		public void AutoMvoeOut()
+		{
+
+		}
+
+	}
+
+}
